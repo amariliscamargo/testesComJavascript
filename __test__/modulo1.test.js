6adc669a429153e64dfb8ba3a84f3d59b7564dcb
Amarilis Camargo <55119@L3-000071.lambda3.com.br>
2019-08-14 11:40:02 -0300

    mudando o teste

--- __test__/modulo1.test.js
+++ __test__/modulo1.test.js
@@ -2,6 +2,6 @@ const modulo1 = require('../modulo1');
 
 describe('Testando as funções do modulo 1',() => {
   it('Esperar que a função FunctionA retorne a soma correta',() => {
-    expect(modulo1.func1(2,2)).toBe(4);
+    expect(modulo1.func1(2,3)).toBe(4);
   })
 });
